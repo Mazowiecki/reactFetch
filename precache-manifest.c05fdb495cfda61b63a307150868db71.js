6e22240b0f0b4929341d8e32662182b784b3bb8e
Stanislaw Mazowiecki <stmazowiecki@gmail.com>
2020-03-21 01:48:51 +0100

    Updates

---
+++ precache-manifest.c05fdb495cfda61b63a307150868db71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c11dfad6226841cfdd96f7d537591e7",
+    "url": "/ghptest/index.html"
+  },
+  {
+    "revision": "0c22c54d57dbe806e277",
+    "url": "/ghptest/static/css/main.d02e64b3.chunk.css"
+  },
+  {
+    "revision": "b02a4ab9134f9ad90563",
+    "url": "/ghptest/static/js/2.506033f2.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/ghptest/static/js/2.506033f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c22c54d57dbe806e277",
+    "url": "/ghptest/static/js/main.3f1707f7.chunk.js"
+  },
+  {
+    "revision": "7328a2704f9f1815dc3e",
+    "url": "/ghptest/static/js/runtime-main.390464d8.js"
+  }
+]);
