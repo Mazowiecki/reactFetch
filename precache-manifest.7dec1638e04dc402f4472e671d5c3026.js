06af1f755f405c5e52240e158deb73075e89b74d
Stanislaw Mazowiecki <stmazowiecki@gmail.com>
2020-03-21 01:56:28 +0100

    Updates

---
+++ precache-manifest.7dec1638e04dc402f4472e671d5c3026.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5097c12356feca80f424ce770ff0c45",
+    "url": "/COVID-19GetDataForCountry/index.html"
+  },
+  {
+    "revision": "83604bcded9662a9c4cd",
+    "url": "/COVID-19GetDataForCountry/static/css/main.d02e64b3.chunk.css"
+  },
+  {
+    "revision": "b02a4ab9134f9ad90563",
+    "url": "/COVID-19GetDataForCountry/static/js/2.506033f2.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/COVID-19GetDataForCountry/static/js/2.506033f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83604bcded9662a9c4cd",
+    "url": "/COVID-19GetDataForCountry/static/js/main.b97ee4fd.chunk.js"
+  },
+  {
+    "revision": "cbca1b6a473a1e2ec273",
+    "url": "/COVID-19GetDataForCountry/static/js/runtime-main.6cdc7e07.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ghptest/precache-manifest.c05fdb495cfda61b63a307150868db71.js"
+  "/COVID-19GetDataForCountry/precache-manifest.7dec1638e04dc402f4472e671d5c3026.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ghptest/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/COVID-19GetDataForCountry/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
